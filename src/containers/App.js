b1458448431a6b97fe1593b948bba49f78c4843c
webersteve <weberwcr@outlook.com>
2022-04-08 22:43:08 -0400

    ImageLinkForm, Rank, TsParticles

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
-import '../style.css';
+import './App.css';
+import TsParticles from '../components/TsParticles/TsParticles';
 import Navigation from '../components/Navigation/Navigation';
 import Logo from '../components/Logo/Logo';
+import Rank from '../components/Rank/Rank';
+import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<TsParticles />	
 				<Navigation />
 				<Logo />
-				{/*<ImageLinkForm />
-				<FaceRecognition />*/}
+				<Rank />
+				<ImageLinkForm />
+				{/*<FaceRecognition />*/}
 			</div>
 		);
 	}
