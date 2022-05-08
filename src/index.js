ec7ad97944a0cbc471447cd281b3b4ada8205cea
webersteve <weberwcr@outlook.com>
2022-05-07 21:16:43 -0400

    update createRoot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
-);
+createRoot(document.getElementById('root')).render(<App />);
